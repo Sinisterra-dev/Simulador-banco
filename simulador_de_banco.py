25088b3534c0f284bb8798a0199f872fa6f8bba5
Alexander Sinisterra <alexander.sinisterra.moreno@gmail.com>
2026-03-10 20:28:09 -0500

    diseño arquitectonico del ejercicio

--- simulador_de_banco.py
+++ simulador_de_banco.py
@@ -9,3 +9,44 @@ class Cuenta:
         self.titular = titular
         self.saldo = saldo
         self.historial = []
+
+    def depositar(self, monto):
+
+        if monto > 0:
+            self.saldo += monto
+            self.historial.append(f"depósito {monto}")
+
+
+    def retirar(self, valor):
+
+        if valor <= 0:
+            print("Ingresa un valor a retirar válido.")
+
+        elif valor > self.saldo:
+            print("Fondos insuficientes")
+
+        else:
+            self.saldo -= valor
+            self.historial.append(f"Retiro: {valor}")
+
+    def ver_historial(self):
+
+        for movimiento in self.historial:
+            print(movimiento) 
+
+class Banco:
+
+    def __init__(self):
+
+        self.cuentas = {}
+
+    def crear_cuenta(self, no_cuenta, titular, saldo):
+        
+
+        if no_cuenta in self.cuentas:
+            print("Cuenta ya existente")
+
+        else:
+            cuenta = Cuenta(no_cuenta, titular, saldo)
+            self.cuentas[no_cuenta] = cuenta
+   
